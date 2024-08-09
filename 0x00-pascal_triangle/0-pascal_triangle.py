2c4d885696b99db48661d9077656b02786214b63
Paul Ogolla <paulotieno2@gmail.com>
2024-08-09 10:56:37 +0300

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,12 @@
+ #!/usr/bin/python3
+"""
+Module for pascal_triangle
+"""
+
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     if n <= 0:
         return []
     triangle = [[1]]
