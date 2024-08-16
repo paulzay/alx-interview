0f71849e23604ec482edc10297ea55dfe945245b
Paul Ogolla <paulotieno2@gmail.com>
2024-08-16 08:54:33 +0300

    document

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
+"""LOCKBOXES"""
+
+
 def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened"""
     if type(boxes) is not list:
         return False
     if boxes == []:
         return False
     if len(boxes) == 0:
         return False
-    
